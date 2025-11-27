0a173f5533f3c95127bb3ddb66a4f0d87de2d78e
Khairul Ikhwan <khairul0355@hotmail.com>
2025-11-28 00:36:35 +0800

    extracts product name, short desc and long desc

--- product_page.py
+++ product_page.py
@@ -4,12 +4,21 @@ from bs4 import BeautifulSoup
 if __name__ == "__main__":
 
     # url = "https://cikgumall.com/product/special-teega-choco-lava-55g/"
-    url = "https://cikgumall.com/product/teega-machos-cheezy-45g/"
+    # url = "https://cikgumall.com/product/teega-machos-cheezy-45g/"
+    # url = "https://cikgumall.com/product/reevo-chocolate-malt-drink-1kg/"
 
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    product = soup.find(attrs={"class":"product_title entry-title"})
+    title = soup.find(attrs={"class":"product_title entry-title"})
+    print(1)
+    print(title.text)
 
-    print(product.text)
+    short_desc = soup.find(attrs={"class":"short-description__content"})
+    print(2)
+    print(short_desc.text)
+
+    long_desc = soup.find(attrs={"class":"motta-dropdown__content"})
+    print(3)
+    print(long_desc)
